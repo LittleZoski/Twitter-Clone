ea10a45ce629d7ba1f1bbbf47f3c4c2aa46f1d25
Brian <bcithal1@gmail.com>
2023-02-11 11:23:37 -0600

    Pulled in Tanstack

--- src/queries/register.queries.ts
+++ src/queries/register.queries.ts
@@ -1,40 +1,75 @@
-import axios from "axios"
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { useRouter } from "next/router";
 
-interface userRegisterInput{
-    username: string,
-    name:string,
-    password:string,
-    workspace:string,
-    termsOfService: boolean
+interface userRegisterInput {
+  username: string;
+  name: string;
+  password: string;
+  workspace: string;
+  termsOfService: boolean;
 }
 
-interface userLoginInput{
-    username: string,
-    password:string
+interface userLoginInput {
+  username: string;
+  password: string;
 }
 
-export const register = async(value: userRegisterInput) =>{
-    const registerPostBody = {
-        username: value.username,
-        name: value.name,
-        password: value.password,
-        workspace: value.workspace
-    }
-
-    try{
-        const response = await axios.post("https://husqr.up.railway.app/register", registerPostBody)
-    }catch(error){
-        console.error(error)
-    }
-
+export function useRegister() {
+  const router = useRouter();
+//   const login = useLogin();
+  return useMutation({
+    mutationFn: (values: userRegisterInput) => {
+      return axios
+        .post("https://husqr.up.railway.app/register", values)
+        .then((response) => response.data);
+    },
+    onSuccess: (data) => login.mutate,
+  });
 }
 
-export const Login = async(values:userLoginInput) => {
-    try{
-        const response = await axios.post("https://husqr.up.railway.app/login", values)
-        console.log(response.data)
-    }catch(error){
-        console.error(error)
-    }
+// export function useLogin() {
 
-}
+//     //pretend please
+//     const router = useRouter();
+  
+//     return useMutation({
+//       mutationFn: (values: userRegisterInput) => {
+//         return axios
+//           .post("https://husqr.up.railway.app/register", values)
+//           .then((response) => response.data);
+//       },
+//       onSuccess: (data) => router.push("/login"),
+//     });
+//   }
+
+
+// export const register = async (value: userRegisterInput) => {
+//   const registerPostBody = {
+//     username: value.username,
+//     name: value.name,
+//     password: value.password,
+//     workspace: value.workspace,
+//   };
+
+//   try {
+//     const response = await axios.post(
+//       "https://husqr.up.railway.app/register",
+//       registerPostBody
+//     );
+//   } catch (error) {
+//     console.error(error);
+//   }
+// };
+
+// export const Login = async (values: userLoginInput) => {
+//   try {
+//     const response = await axios.post(
+//       "https://husqr.up.railway.app/login",
+//       values
+//     );
+//     console.log(response.data);
+//   } catch (error) {
+//     console.error(error);
+//   }
+};
