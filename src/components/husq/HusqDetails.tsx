49afbd36adb7756643cd8c57dae635e9fc428e6c
Jenna Swaney <jennalswaney@gmail.com>
2023-02-16 19:58:17 -0700

    Like button

--- src/components/husq/HusqDetails.tsx
+++ src/components/husq/HusqDetails.tsx
@@ -8,13 +8,14 @@ import LikeHusq from "./LikeHusq";
 function HusqDetails(husq: Husq) {
   //we're getting the authorId from the user. We can use that id to pull username from the user type.
 
-  const user = usegetUserWithId(husq.authorId!)
-  
+  const user = usegetUserWithId(husq.authorId!);
+  const husqId = husq.id;
+
   return (
     <div>
       <div>{user?.username}</div>
       <div>{husq.text}</div>
-      <LikeHusq></LikeHusq>
+      <LikeHusq id={husqId} />
     </div>
   );
 }
