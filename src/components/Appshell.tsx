a5b35940588cbe7c17c798d4e39d9ba1d88ec1e1
Darla Pybas <darla.pybas2000@gmail.com>
2023-02-16 18:33:17 -0600

    Login styling and some homepage

--- src/components/Appshell.tsx
+++ src/components/Appshell.tsx
@@ -9,10 +9,21 @@ import {
   MediaQuery,
   Burger,
   useMantineTheme,
+  Center,
 } from '@mantine/core';
 import { useQuery } from '@tanstack/react-query';
-import axios from 'axios';
 import { API } from '@/api/api';
+import {AiOutlineTwitter} from 'react-icons/ai';
+import {CgMoreO} from 'react-icons/cg';
+import {BsSearch, BsFillPersonFill} from 'react-icons/bs';
+import {FiBell} from 'react-icons/fi';
+import {GoVerified} from 'react-icons/go';
+import {RiHome7Fill} from 'react-icons/ri';
+import {MdPersonOutline} from 'react-icons/md';
+import {BiHash, BiEnvelope, BiBookmark} from  'react-icons/bi';
+import Link from 'next/link';
+
+<script src="https://kit.fontawesome.com/43dcc20e7a.js" crossOrigin="anonymous"></script>
 
 export default function AppShellPage() {
   const theme = useMantineTheme();
@@ -36,8 +47,28 @@ export default function AppShellPage() {
       navbarOffsetBreakpoint="sm"
       asideOffsetBreakpoint="sm"
       navbar={
-        <Navbar p="md" hiddenBreakpoint="sm" hidden={!opened} width={{ sm: 200, lg: 300 }}>
-          <Text>Application navbar</Text>
+        <Navbar p="lg" hiddenBreakpoint="sm" width={{ sm: 600, lg: 600 }}>
+          <div className='nav'>
+          <nav>
+            <Link className="link" href="/">
+              <i className='nav-icons'><RiHome7Fill /></i> Home</Link>
+            <Link className="link" href="/explore">
+              <i className='nav-icons'><BiHash /></i> Explore</Link>
+            <Link className="link" href="/notifications">
+              <i className='nav-icons'><FiBell /></i> Notifications</Link>
+            <Link className="link" href="/messages">
+              <i className='nav-icons'><BiEnvelope /></i> Messages</Link>
+            <Link className="link" href="/bookmarks">
+              <i className='nav-icons'><BiBookmark /></i> Bookmarks</Link>
+            <Link className="link" href="/blue">
+              <i className='nav-icons'><GoVerified /></i> Blue</Link>
+            <Link className="link" href="/profile">
+              <i className='nav-icons'><MdPersonOutline /></i> Profile</Link>
+            <Link className="link" href="/more">
+              <i className='nav-icons'><CgMoreO /></i> More</Link>
+            <button className='tweet'>Husq</button>
+          </nav>
+          </div>
         </Navbar>
       }
       aside={
@@ -46,11 +77,6 @@ export default function AppShellPage() {
             <Text>Application sidebar</Text>
           </Aside>
         </MediaQuery>
-      }
-      footer={
-        <Footer height={60} p="md">
-          Application footer
-        </Footer>
       }
       header={
         <Header height={{ base: 50, md: 70 }} p="md">
@@ -64,8 +90,19 @@ export default function AppShellPage() {
                 mr="xl"
               />
             </MediaQuery>
-
-            <Text>Application header</Text>
+            <div className='icon'>
+              <AiOutlineTwitter size='3em'
+              style={{color: '#00acee'}}/>
+            </div>
+            <Text><h1>Husqr</h1></Text>
+            <div className="container">
+            <div className="search-box">
+              <input type="text" className="search-input" placeholder="Search Husqr"></input>
+              <button className="search-button">
+              <i><BsSearch/></i>
+              </button>
+            </div>
+            </div>
           </div>
         </Header>
       }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,15 @@
+import Link from "next/link";
 import React from "react";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className='nav'>
+    <nav>
+        <Link className="link" href="/">Home</Link>
+        <Link className="link" href="/listings">Listings</Link>
+    </nav>
+    </div>
+  )
 }
 
 export default Navbar;
