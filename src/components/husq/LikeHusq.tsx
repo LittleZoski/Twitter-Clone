0a5592eade96e9c8b28d354f3e8e69ee5481b858
Jenna <jennalswaney@gmail.com>
2023-02-23 17:31:38 -0600

    like display fix

--- src/components/husq/LikeHusq.tsx
+++ src/components/husq/LikeHusq.tsx
@@ -1,56 +1,62 @@
 import { useDeleteLike, useLikeHusq } from "@/queries/husq.queries";
 import React, { useState } from "react";
-import { UnstyledButton, Group, Avatar, Text } from "@mantine/core";
+import { UnstyledButton, Group, Avatar, Text, ActionIcon } from "@mantine/core";
 import { BsHeart, BsHeartFill } from "react-icons/bs";
 import { Husq } from "@/types/husq";
 import { useGetCurrentUser } from "@/queries/user.queries";
 
 const LikePost = ({ liked, likes }: { liked: Boolean; likes: number }) => {
-  return (
-    <>
-      <Group>
-        {liked ? <BsHeartFill color="#00acee" /> : <BsHeart color="#00acee" />}
-        <Text>{likes}</Text>
-      </Group>
-    </>
-  );
+	return (
+		<>
+			<Group spacing={"xs"}>
+				<ActionIcon>
+					{liked ? (
+						<BsHeartFill color="#00acee" />
+					) : (
+						<BsHeart color="#00acee" />
+					)}
+				</ActionIcon>
+				<Text> {likes}</Text>
+			</Group>
+		</>
+	);
 };
 
 const DisplayLikes = ({ likes }: { likes: number }) => {
-  return <Text fw={700}>Likes: {likes}</Text>;
+	return <Text fw={700}>Likes: {likes}</Text>;
 };
 
 function LikeHusq({ husq }: { husq: Husq }) {
-  //need to add useState for liked unliked status onClick
-  const likeHusq = useLikeHusq();
-  const currentUser = useGetCurrentUser().userId!;
-  const unLikeHusq = useDeleteLike(currentUser);
-  const showLikeButton = husq.authorId != currentUser;
+	//need to add useState for liked unliked status onClick
+	const likeHusq = useLikeHusq();
+	const currentUser = useGetCurrentUser().userId!;
+	const unLikeHusq = useDeleteLike(currentUser);
+	const showLikeButton = husq.authorId != currentUser;
 
-  const [liked, setLiked] = useState(husq.liked);
+	const [liked, setLiked] = useState(husq.liked);
 
-  function handleClick() {
-    if (liked === false) {
-      setLiked(true);
-      husq._count.likes += 1;
-      likeHusq.mutate(husq.id);
-    } else {
-      setLiked(false);
-      husq._count.likes -= 1;
-      unLikeHusq.mutate(husq.id);
-    }
-  }
-  return (
-    <Group>
-      {showLikeButton ? (
-        <UnstyledButton onClick={handleClick}>
-          <LikePost liked={liked} likes={husq._count.likes} />
-        </UnstyledButton>
-      ) : (
-        <DisplayLikes likes={husq._count.likes} />
-      )}
-    </Group>
-  );
+	function handleClick() {
+		if (liked === false) {
+			setLiked(true);
+			husq._count.likes += 1;
+			likeHusq.mutate(husq.id);
+		} else {
+			setLiked(false);
+			husq._count.likes -= 1;
+			unLikeHusq.mutate(husq.id);
+		}
+	}
+	return (
+		<Group>
+			{showLikeButton ? (
+				<UnstyledButton onClick={handleClick}>
+					<LikePost liked={liked} likes={husq._count.likes} />
+				</UnstyledButton>
+			) : (
+				<DisplayLikes likes={husq._count.likes} />
+			)}
+		</Group>
+	);
 }
 
 export default LikeHusq;
